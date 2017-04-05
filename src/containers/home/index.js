4c38fb13c6ef78bee21f0e706a4d0d7b81f2c798
Jamie Barton <hi_jamie@me.com>
2017-04-05 10:41:43 +0100

    CSS

--- src/containers/home/index.js
+++ src/containers/home/index.js
@@ -15,12 +15,12 @@ const Home = props => (
     <p>Count: {props.count}</p>
 
     <p>
-      <button onClick={props.increment}>Increment</button>
+      <button onClick={props.increment} disabled={props.isIncrementing}>Increment</button>
       <button onClick={props.incrementAsync} disabled={props.isIncrementing}>Increment Async</button>
     </p>
 
     <p>
-      <button onClick={props.decrement}>Decrementing</button>
+      <button onClick={props.decrement} disabled={props.isDecrementing}>Decrementing</button>
       <button onClick={props.decrementAsync} disabled={props.isDecrementing}>Decrement Async</button>
     </p>
 
