d5fc2dd762290864c4d806bc452eeba0d34bec17
adamchenwei <awei@pebblepost.com>
2018-03-23 17:50:56 -0400

    build(dev tool): update config to latest

--- src/store.js
+++ src/store.js
@@ -3,6 +3,7 @@ import { routerMiddleware } from 'react-router-redux';
 import thunk from 'redux-thunk';
 import createHistory from 'history/createBrowserHistory';
 import rootReducer from './modules';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 export const history = createHistory();
 
@@ -11,7 +12,7 @@ const enhancers = [];
 const middleware = [thunk, routerMiddleware(history)];
 
 if (process.env.NODE_ENV === 'development') {
-  const devToolsExtension = window.devToolsExtension;
+  const devToolsExtension = window.__REDUX_DEVTOOLS_EXTENSION__;
 
   if (typeof devToolsExtension === 'function') {
     enhancers.push(devToolsExtension());
