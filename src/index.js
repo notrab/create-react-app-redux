bf795326e14ca97cf8d74e32df174b22e3ab8bac
David Gutierrez McCormack <davidmccormack2000@gmail.com>
2019-06-12 20:42:33 -0400

    Removed unecessary counter.js file and refractored component folder and files.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import { ConnectedRouter } from 'connected-react-router'
 import store, { history } from './store'
-import App from './containers/app'
+import App from './components/app/index'
 
 import 'sanitize.css/sanitize.css'
 import './index.css'
