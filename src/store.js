ef47ac1999842f063f66345273e11847f6cca1da
Abhay <pratapabhay540@gmail.com>
2019-08-07 19:26:50 +0530

    added browserlist in package.json & improved import for createBrowserHistory() (#112)

--- src/store.js
+++ src/store.js
@@ -1,10 +1,10 @@
 import { createStore, applyMiddleware, compose } from 'redux'
 import { connectRouter, routerMiddleware } from 'connected-react-router'
 import thunk from 'redux-thunk'
-import createHistory from 'history/createBrowserHistory'
+import * as History from 'history'
 import rootReducer from './modules'
 
-export const history = createHistory()
+export const history = History.createBrowserHistory()
 
 const initialState = {}
 const enhancers = []
